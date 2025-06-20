35f5d7936c2e5684afaf0a3a5a75fb594ca7a28f
ArchivedPumpkin <106057361+ArchivedPumpkin@users.noreply.github.com>
2025-06-20 23:39:11 +0200

    Add initial HTML structure and JavaScript functionality for password generation

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const lengthInput = document.getElementById("length")
+const generateBtn = document.getElementById("generate")
+const generatedPassword = document.getElementById("generated-password")
+const copyBtn = document.getElementById("copy")
+
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
     "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6",
     "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[",
     "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
-    "/"];
+    "/"]
 
-const passwordLength = 12
+const passwordLength = lengthInput.value
 
 function generateRandomCharacter() {
     const randomIndex = Math.floor(Math.random() * characters.length)
@@ -19,6 +24,10 @@ function generatePassword(length) {
     }
     return password
 }
+
+generateBtn.addEventListener("click", function () {
+    console.log("clicked")
+})
 
 console.log("Random password: " + generatePassword(passwordLength))
 
